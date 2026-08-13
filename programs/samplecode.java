5458bb3caa98f6bfa554191873154ff17eac7f8a
devrajs144 <devraj.s@ahduni.edu.in>
2026-08-13 15:47:42 +0530

    fixed the code

--- programs/samplecode.java
+++ programs/samplecode.java
@@ -1,23 +1,14 @@
-import javax.swing.*;
+
 import java.awt.*;
+import javax.swing.*;
 
 public class samplecode extends JPanel {
 
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // =========================
-        // LINE
-        // =========================
         Graphics2D g2 = (Graphics2D) g;
-
-        // Line color and width
-        g2.setColor(Color.RED);
-        g2.setStroke(new BasicStroke(5));
-
-        // Draw line
-        g2.drawLine(50, 50, 250, 50);
-
 
         // =========================
         // SQUARE
@@ -27,41 +18,7 @@ public class samplecode extends JPanel {
 
         // Draw square
         g2.drawRect(50, 100, 100, 100);
-
-
-        // =========================
-        // RECTANGLE
-        // =========================
-        g2.setColor(Color.GREEN);
-        g2.setStroke(new BasicStroke(6));
-
-        // Draw rectangle
-        g2.drawRect(200, 100, 150, 80);
-
-
-        // =========================
-        // CIRCLE
-        // =========================
-        g2.setColor(Color.ORANGE);
-        g2.setStroke(new BasicStroke(3));
-
-        // Draw circle
-        g2.drawOval(50, 250, 100, 100);
-
-
-        // =========================
-        // TRIANGLE
-        // =========================
-        g2.setColor(Color.MAGENTA);
-        g2.setStroke(new BasicStroke(5));
-
-        int[] x = {250, 200, 300};
-        int[] y = {230, 330, 330};
-
-        // Draw triangle
-        g2.drawPolygon(x, y, 3);
     }
-
 
     public static void main(String[] args) {
 
@@ -78,4 +35,3 @@ public class samplecode extends JPanel {
         frame.setVisible(true);
     }
 }
-
